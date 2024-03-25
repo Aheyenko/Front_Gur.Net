a176d13a68db124a98fc28571e132ae78ba2432c
OleksandraHuk <144731321+OleksandraHuk@users.noreply.github.com>
2024-03-25 22:58:17 +0200

    Add logging and sql logic

---
+++ GurNet.API/models/Response.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace GurNet.API.models
+{
+    public class Response
+    {
+        public int statusCode { get; set; }
+        public string statusMessage { get; set; }
+
+    }
+}
