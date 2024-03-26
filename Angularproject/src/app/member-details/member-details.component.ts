45a3d0a3051b59dad5a0be0437ccec9b4c4ef7a6
Anastasiia.Aheienko.PM.2022@lpnu.ua <Anastasiia.Aheienko.PM.2022@lpnu.ua>
2024-03-26 21:16:58 +0200

    FakeData

--- Angularproject/src/app/member-details/member-details.component.ts
+++ Angularproject/src/app/member-details/member-details.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../_services/users.service';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-member-details',
   templateUrl: './member-details.component.html',
   styleUrl: './member-details.component.scss'
 })
-export class MemberDetailsComponent {
+export class MemberDetailsComponent implements OnInit {
 
+  user: User | null = null;
+
+  constructor(private usersService: UsersService) {
+
+  }
+
+  ngOnInit(): void {
+    this.user = this.generateUserFakeData();
+    // this.getUserById();
+  }
+
+  getUserById() {
+    this.usersService.getUser("userId").subscribe((user) => {
+      console.log(user);
+      if(user) {
+        this.user = user;
+      }
+    }, (error) => {
+      console.log(error);
+      this.user = this.generateUserFakeData();
+    });
+  }
+
+  private generateUserFakeData() {
+    let user: User = {
+     id:"userId",
+     firstName: "Mike",  
+     userName: "Wazovski",  
+     middleName: "Vitaliovich",  
+    };
+
+    return user;
+  }
 }
