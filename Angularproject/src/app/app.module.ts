4433eb7d866655aa543a2cc73e128467aaea3113
Anastasiia.Aheienko.PM.2022@lpnu.ua <Anastasiia.Aheienko.PM.2022@lpnu.ua>
2024-03-26 21:02:28 +0200

    Added routing

--- Angularproject/src/app/app.module.ts
+++ Angularproject/src/app/app.module.ts
@@ -5,14 +5,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { MemberDetailsComponent } from './member-details/member-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    MemberDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -20,6 +21,7 @@ import { LoginFormComponent } from './login-form/login-form.component';
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    RouterModule
   ],
   providers: [ AuthService],
   bootstrap: [AppComponent]
--- Angularproject/src/app/member-details/member-details.component.spec.ts
+++ Angularproject/src/app/member-details/member-details.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { MemberDetailsComponent } from './member-details.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('MemberDetailsComponent', () => {
+  let component: MemberDetailsComponent;
+  let fixture: ComponentFixture<MemberDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      declarations: [MemberDetailsComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(MemberDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
