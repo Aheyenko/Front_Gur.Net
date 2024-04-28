68737d0d3de523a98cbc4e9b449383a457f0c979
Anastasiia.Aheienko.PM.2022@lpnu.ua <Anastasiia.Aheienko.PM.2022@lpnu.ua>
2024-04-28 19:56:56 +0300

    Adapted logo

--- Angularproject/src/app/login-form/login-form.component.ts
+++ Angularproject/src/app/login-form/login-form.component.ts
@@ -18,33 +18,33 @@ export class LoginFormComponent {
   constructor(private authService: AuthService, private router: Router, private http: HttpClient) { } // Впровадження AuthService в компонент
 
   onSubmit(): void {
-    this.http.post('https://localhost:5001/api/login', {
-      email: this.email,
-      password: this.identificationCode
-    }).subscribe((res: any) => {
-      // if (res.status === 200) {
-        this.router.navigate(['/member-details/dsfsd']);
-      // }
-      console.log(res);
-    }, (error) => {
-      alert('not ok');
-      console.log(error);
-    });
-
-    // axios.post('https://localhost:5001/api/login', {
+    // this.http.post('http://localhost:4200/', {
     //   email: this.email,
     //   password: this.identificationCode
-    // })
-    // .then(r => {
-    //   if (r.status === 200)
+    // }).subscribe((res: any) => {
+    //   // if (res.status === 200) {
     //     this.router.navigate(['/member-details/dsfsd']);
-    //   else
-    //     alert('not ok');
+    //   // }
+    //   console.log(res);
+    // }, (error) => {
+    //   alert('not ok');
+    //   console.log(error);
     // });
+
+    axios.post('http://localhost:4200/', {
+      email: this.email,
+      password: this.identificationCode
+    })
+    .then(r => {
+      if (r.status === 200)
+        this.router.navigate(['/member-details/dsfsd']);
+      else
+        alert('not ok');
+    });
     // Виконуємо перевірку введених даних перед відправкою на сервер
-    if (!this.email || !this.identificationCode) {
-      this.errorMessage = 'Будь ласка, заповніть всі поля'; // Відображаємо повідомлення про неповність даних
-      return;
-    }
+    // if (!this.email || !this.identificationCode) {
+    //   this.errorMessage = 'Будь ласка, заповніть всі поля'; // Відображаємо повідомлення про неповність даних
+    //   return;
+    // }
   }
 }
