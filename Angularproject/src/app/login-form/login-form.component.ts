17cae75231575b7e8981426c02aa54308d44e14d
Anastasiia.Aheienko.PM.2022@lpnu.ua <Anastasiia.Aheienko.PM.2022@lpnu.ua>
2024-04-28 19:30:35 +0300

    Addapting  logo

--- Angularproject/src/app/login-form/login-form.component.ts
+++ Angularproject/src/app/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from "@angular/router"
 import { AuthService } from '../auth.service';
 import axios from 'axios';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -14,19 +15,32 @@ export class LoginFormComponent {
   email: string = '';
   errorMessage: string = ''; // змінна для відображення помилок
 
-  constructor(private authService: AuthService, private router: Router) { } // Впровадження AuthService в компонент
+  constructor(private authService: AuthService, private router: Router, private http: HttpClient) { } // Впровадження AuthService в компонент
 
   onSubmit(): void {
-    axios.post('http://localhost:4200/', {
+    this.http.post('https://localhost:5001/api/login', {
       email: this.email,
       password: this.identificationCode
-    })
-    .then(r => {
-      if (r.status === 200)
+    }).subscribe((res: any) => {
+      // if (res.status === 200) {
         this.router.navigate(['/member-details/dsfsd']);
-      else
-        alert('not ok');
+      // }
+      console.log(res);
+    }, (error) => {
+      alert('not ok');
+      console.log(error);
     });
+
+    // axios.post('https://localhost:5001/api/login', {
+    //   email: this.email,
+    //   password: this.identificationCode
+    // })
+    // .then(r => {
+    //   if (r.status === 200)
+    //     this.router.navigate(['/member-details/dsfsd']);
+    //   else
+    //     alert('not ok');
+    // });
     // Виконуємо перевірку введених даних перед відправкою на сервер
     if (!this.email || !this.identificationCode) {
       this.errorMessage = 'Будь ласка, заповніть всі поля'; // Відображаємо повідомлення про неповність даних
@@ -34,4 +48,3 @@ export class LoginFormComponent {
     }
   }
 }
-
